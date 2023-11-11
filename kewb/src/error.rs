1f2dd1ddc9893e4406f6bbde73a75ed79cb4768e
Luckas <luckasranarison@gmail.com>
2023-11-11 11:55:06 +0300

    update: use thiserror
    Use thiserror for implementing the `Error` trait.

--- kewb/src/error.rs
+++ kewb/src/error.rs
@@ -1,51 +1,27 @@
 use bincode::error::{DecodeError, EncodeError};
-use core::fmt;
 use std::io;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
+    #[error("Invalid color value")]
     InvalidColor,
+    #[error("Invalid edge value")]
     InvalidEdge,
+    #[error("Invalid corner value")]
     InvalidCorner,
+    #[error("Invalid scramble string")]
     InvalidScramble,
+    #[error("Invalid facelet string")]
     InvalidFaceletString,
+    #[error("Invalid facelet reperesentation")]
     InvalidFaceletValue,
+    #[error("Invalid cubie reperesentation")]
     InvalidCubieValue,
-    IOError(io::Error),
-    DecodeError(DecodeError),
-    EncodeError(EncodeError),
-}
-
-impl From<io::Error> for Error {
-    fn from(value: io::Error) -> Self {
-        Self::IOError(value)
-    }
-}
-
-impl From<DecodeError> for Error {
-    fn from(value: DecodeError) -> Self {
-        Self::DecodeError(value)
-    }
-}
-
-impl From<EncodeError> for Error {
-    fn from(value: EncodeError) -> Self {
-        Self::EncodeError(value)
-    }
-}
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::InvalidColor => write!(f, "Invalid color value"),
-            Self::InvalidEdge => write!(f, "Invalid edge value"),
-            Self::InvalidCorner => write!(f, "Invalid corner value"),
-            Self::InvalidScramble => write!(f, "Invalid scramble"),
-            Self::InvalidFaceletString => write!(f, "Invalid facelet string"),
-            Self::InvalidFaceletValue => write!(f, "Invalid facelet reperesentation"),
-            Self::InvalidCubieValue => write!(f, "Invalid cubie reperesentation"),
-            Self::IOError(value) => value.fmt(f),
-            Self::DecodeError(value) => value.fmt(f),
-            Self::EncodeError(value) => value.fmt(f),
-        }
-    }
+    #[error("{0}")]
+    IOError(#[from] io::Error),
+    #[error("{0}")]
+    DecodeError(#[from] DecodeError),
+    #[error("{0}")]
+    EncodeError(#[from] EncodeError),
 }
